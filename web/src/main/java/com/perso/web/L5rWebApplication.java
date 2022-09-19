cde03ba796378711516b02f65e5d264c2fd35a50
hervemeritis <herve.letourneur@meritis.fr>
2022-09-19 23:18:10 +0200

    Add directory

---
+++ web/src/main/java/com/perso/web/L5rWebApplication.java
@@ -0,0 +1,13 @@
+package com.perso.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L5rWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L5rWebApplication.class, args);
+	}
+
+}
---
+++ web/src/test/java/com/perso/web/L5rWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perso.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L5rWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
